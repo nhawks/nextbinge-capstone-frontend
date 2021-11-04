248f87b2937e91ed2737fd890a681f7c0c017fd2
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-04 16:43:36 -0400

    Initial creation of UserLogin component.

---
+++ src/components/UserLogin/UserLogin.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap/Form';
+import { 
+    MDBInput,
+    MDBBtn,
+} from 'mdb-react-ui-kit';
+
+
+function UserLogin(props) {
+    const [loginValues, setLoginValues] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleChange = (e) => {
+        setLoginValues({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const loginUser = (e) => {
+        e.preventDefault()
+        props.login(loginValues)
+    }
+
+
+    return (
+        <React.Fragment>
+            <Form className="my-auto" onSubmit={loginUser}>
+                <Form.Group controlId="loginFormUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control 
+                        name="username" 
+                        value={loginValues.username}
+                        onChange={handleChange}
+                        type="text" 
+                        placeholder="Enter username"
+                    />
+                </Form.Group>
+                <Form.Group controlId="loginFormPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        name="password"
+                        value={loginValues.password}
+                        onChange={handleChange}
+                        type="password" 
+                        placeholder="Enter password" 
+                    />
+                </Form.Group>
+                <MDBBtn className="btn" color="primary" type="submit">
+                    Login
+                </MDBBtn>
+            </Form>
+        </React.Fragment> 
+    );
+}
+
+export default UserLogin;
