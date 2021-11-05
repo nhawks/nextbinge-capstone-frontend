23e59d50de6640700e1359316b45762f682bc9c8
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-05 15:14:53 -0400

    Initial creation of page.

---
+++ src/components/PageAccess/PageAccess.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const PageAccess = (props) => {
+    return (
+        <div>
+            Access - Login/Sign Up
+        </div>
+    )
+
+}
+
+
+export default PageAccess;
