d516021ac2e355b9962b55b9195cde12da601e04
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-05 15:33:05 -0400

    Added imports for new page components and added Switch and Routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { Redirect, Switch, Route } from 'react-router';
 import jwtDecode from 'jwt-decode';
-import axios from 'axios';
+// import axios from 'axios';
+
+import NavBar from './components/NavBar/NavBar';
+import PageHome from './components/PageHome/PageHome';
+import PageAccess from './components/PageAccess/PageAccess';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import PageAccount from './components/PageAccount/PageAccount';
+import PageSearch from './components/PageSearch/PageSearch';
+
 import './App.css'
 
 
@@ -29,7 +37,56 @@ class App extends Component {
     const user = this.state.user 
     return ( 
       <div>
-       
+        <NavBar user={user} />
+        <Switch>
+
+          <Route path="/" exact component={PageHome} />
+
+          {/* Access Page - AllowAny */}
+          <Route path="/access" component={PageAccess} />
+
+          {/* Not-Found Page - AllowAny */}
+          <Route path="/not-found" component={PageNotFound} />
+          
+          {/* Home Page - Users Only */}
+          <Route 
+            path="/home"
+            render={props => {
+              if(!user) {
+                return <Redirect to="/access" />
+              } else {
+                return <PageHome {...props} />
+              }
+            }}
+          />
+
+          {/* Account Page - Users Only */}
+          <Route 
+            path="/account"
+            render={props => {
+              if(!user) {
+                return <Redirect to="/access" />
+              } else {
+                return <PageAccount {...props} />
+              }
+            }}
+          />
+
+          {/* Search Page - Users Only */}
+          <Route 
+            path="/search"
+            render={props => {
+              if(!user) {
+                return <Redirect to="/access" />
+              } else {
+                return <PageSearch {...props} />
+              }
+            }}
+          />
+         
+          <Redirect to='/not-found' />
+
+        </Switch>
       </div>
     );
   }
