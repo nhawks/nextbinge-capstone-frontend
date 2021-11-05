c73533ba46245f2ad8e632f45c45e314f803a414
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-05 00:33:08 -0400

    Added User Register component and necessary imports.

--- src/components/UserAccess/UserAccess.jsx
+++ src/components/UserAccess/UserAccess.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import UserLogin from '../UserLogin/UserLogin';
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText } from 'mdb-react-ui-kit';
+import UserRegister from '../UserRegister/UserRegister';
+import {Tabs, Tab } from 'react-bootstrap/'
+import { MDBCard, MDBCardBody, MDBCardTitle } from 'mdb-react-ui-kit';
+
 
 function UserAccess(props) {
     return (
         <div>
-        <MDBCard style={{ maxWidth: '50rem' }}>
-            <MDBCardBody>
-                <MDBCardTitle>Login</MDBCardTitle>
-                <MDBCardText>
-                    <UserLogin {...props} />
-                </MDBCardText>
-            </MDBCardBody>
-        </MDBCard>
+        <Tabs defaultActiveKey="signUp" className="mb-3">
+            <Tab eventKey="login" title="Login">
+                <MDBCard style={{ maxWidth: '30rem' }}>
+                    <MDBCardBody>
+                        <MDBCardTitle>Login</MDBCardTitle>
+                            <UserLogin {...props} />
+                    </MDBCardBody>
+                </MDBCard>
+            </Tab>
+            <Tab eventKey="signUp" title="Sign Up">
+                <MDBCard style={{ maxWidth: '35rem' }}>
+                    <MDBCardBody>
+                        <MDBCardTitle>Sign Up</MDBCardTitle>
+                            <UserRegister {...props} />
+                    </MDBCardBody>
+                </MDBCard>
+            </Tab>
+        </Tabs>
         </div>
     );
 }
