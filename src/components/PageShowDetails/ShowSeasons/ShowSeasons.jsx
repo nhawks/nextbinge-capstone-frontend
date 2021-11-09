c9d6aadda2b585870904f786288b95947e780d20
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-09 00:08:53 -0500

    Added required imports and format for displaying seasons.

--- src/components/PageShowDetails/ShowSeasons/ShowSeasons.jsx
+++ src/components/PageShowDetails/ShowSeasons/ShowSeasons.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { ListGroupItem, Image } from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
 import ListGroup from 'react-bootstrap/ListGroup';
+
+import { ListGroupItem, Image, Row, Col } from 'react-bootstrap';
+
 
 const ShowSeasons = ({ show }) => {
 
@@ -16,20 +18,31 @@ const ShowSeasons = ({ show }) => {
                 </Accordion.Header>
                 <Accordion.Body>
                     <ListGroup>
-                        {/*//TODO: Map through number of seasons and display description */}
-                        {seasons.map((season => (
-                            <ListGroupItem>
-                                <div className="d-flex w-100 justify-content-between">
-                                    <h5 className="mb-1">{season.name}</h5>
-                                    <small>Season {season.season_number} | Episodes {season.episode_count}</small>
-                                </div>
-                                <p className="mb-1 text-start">
-                                    {season.overview}
-                                </p>
-                            </ListGroupItem>
-
-                                
-                        )))}
+                    {seasons.map(season => (
+                        <ListGroupItem className="mt-2">
+                            <Row className="g-0">
+                                <Col md="3" className="me-4">
+                                    <Image
+                                        src={`https://image.tmdb.org/t/p/w500/${season.poster_path}`}
+                                        alt={`${show.name}`} 
+                                        fluid
+                                        rounded
+                                        className="img-fluid"
+                                    />
+                                </Col>
+                                <Col md="8">
+                                    <div className="d-flex w-100 justify-content-between">
+                                        <h5 className="mb-1">{season.name}</h5>
+                                        <small>Season {season.season_number} | Episodes {season.episode_count}</small>
+                                    </div>
+                                    <hr />
+                                    <p className="mb-1 text-start">
+                                        {season.overview}
+                                    </p>
+                                </Col>
+                            </Row>
+                        </ListGroupItem>
+                    ))}
                     </ListGroup>
                 </Accordion.Body>
             </Accordion.Item>
