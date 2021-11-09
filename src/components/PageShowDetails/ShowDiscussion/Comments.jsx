3be6ee090c63213f97b487401b9ad1e6f8cfd371
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-09 11:45:11 -0500

    Update Comments.jsx
    - Imported replies component and passed props.
    - Added userComments object from discussion prop.

--- src/components/PageShowDetails/ShowDiscussion/Comments.jsx
+++ src/components/PageShowDetails/ShowDiscussion/Comments.jsx
@@ -1,42 +1,44 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import Replies from './Replies';
 
 
 function Comments (props) {
+
+    const userComments = props.discussion[0]
 
     return ( 
         <div className = "row">
             <div className = "col-md-12">
                 <h5 className="text-start p-2">
-                    {props.userComments ? "See what others have to say!" : "Start the conversation!"}
+                    {userComments ? "See what others have to say!" : "Start the conversation!"}
                 </h5>
-                <ListGroup>
-                {props.userComments.map((comment) => {
-                    return (   
-                        <ListGroupItem key={`${comment.id}`} className="border border-primary">
-                            <Card className="">
-                                <Card.Header className="text-start p-2">
-                                    <div className="row row-cols-auto fs-5">
-                                        <div className="col">
-                                            <i className="bi bi-person-circle p-2"></i>
-                                            {comment.username}
-                                        </div>
-                                        <div className="col">
-                                            {/*//TODO: Like & Dislike Comment */}
-                                        </div>
+                {userComments.map((comment) => {
+                    return (
+                        <>   
+                        <Card className="mb-3">
+                            <Card.Header className="text-start p-2">
+                                <div className="row row-cols-auto fs-5">
+                                    <div className="col">
+                                        <i className="bi bi-person-circle p-2"></i>
+                                        {comment.username}
                                     </div>
-                                </Card.Header>
-                                <Card.Body>
-                                    <p className="d-flex justify-content-start">
-                                        {comment.message}
-                                    </p>    
-                                </Card.Body>
-                            </Card>
-                        </ListGroupItem>
+                                    <div className="col">
+                                        {/*//TODO: Like & Dislike Comment */}
+                                    </div>
+                                </div>
+                            </Card.Header>
+                            <Card.Body>
+                                <p className="d-flex justify-content-start">
+                                    {comment.message}
+                                </p>    
+                            </Card.Body>
+                        <Replies {...props} commentOP={comment.username} commentID={comment.id} />
+                        </Card>
+                        </>
                     )
                 })}
-                </ListGroup>
             </div>
         </div>
     );
