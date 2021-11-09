15454f9bd99a13f2460f7d4aaa3c721f5d694d49
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-09 00:09:08 -0500

    Added aysnc and await to axios call.

--- src/components/PageShowDetails/ShowDetails/ShowDetails.jsx
+++ src/components/PageShowDetails/ShowDetails/ShowDetails.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { TMDB_API_KEY } from '../../../keys';
+
 import ShowSeasons from '../ShowSeasons/ShowSeasons';
 
 import { Card, Row, Col, Spinner} from 'react-bootstrap';
-import Accordion from 'react-bootstrap/Accordion';
+
 
 const ShowDetails = (props) => {
     const [isLoading, setLoading] = useState(true)
@@ -12,18 +13,21 @@ const ShowDetails = (props) => {
 
     useEffect(() => {
         try {
-            axios.get(`https://api.themoviedb.org/3/tv/${props.tv_id}`,
-            {params: {
-                api_key: TMDB_API_KEY,
-                language: "en-US"
-            }})
-            .then(response => {
+            const getShow = async () => {
+                const response = await axios.get(`https://api.themoviedb.org/3/tv/${props.tv_id}`,
+                {params: {
+                    api_key: TMDB_API_KEY,
+                    language: "en-US"
+                }})
                 setShow(response.data)
                 setLoading(false)
-            })
+            }
+            getShow()
         } catch(err) {
             console.log("🚀 ~ file: ShowDetails.jsx ~ line 24 ~ useEffect ~ err", err)
         }
+
+        
     }, []);
 
     if (isLoading) {
@@ -35,16 +39,16 @@ const ShowDetails = (props) => {
     }
 
     return (
-        <Card style={{ maxWidth: '75rem' }}>
-            <Row className='g-0'>
-                <Col md='4'>
+        <Card style={{ maxWidth: "75rem" }}>
+            <Row >
+                <Col md="4">
                 <Card.Img 
                     src={`https://image.tmdb.org/t/p/w500/${show.poster_path}`}
                     alt={`${show.name}`} 
-                    fluid 
+                    fluid
                 />
                 </Col>
-                <Col md='8'>
+                <Col md="8">
                 <Card.Body>
                     <Card.Title>{show.name} ({show.first_air_date.slice(0, 4)})</Card.Title>
                     <Card.Text className="text-start lh-lg">
@@ -58,6 +62,7 @@ const ShowDetails = (props) => {
                     <Card.Text className="text-muted">
                         Show Status: {show.status}
                     </Card.Text>
+                    {/*//TODO: Add pagination to seasons accordion */}
                     <ShowSeasons show={show} />
                 </Card.Body>
                 </Col>
