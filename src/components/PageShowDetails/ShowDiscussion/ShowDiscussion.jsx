4dff21fa4140e52d371259d52175ac17f6da97da
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-09 03:03:37 -0500

    Created axios request to retrieve all comments/replies.

--- src/components/PageShowDetails/ShowDiscussion/ShowDiscussion.jsx
+++ src/components/PageShowDetails/ShowDiscussion/ShowDiscussion.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Card, Spinner } from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
 import DiscussionForm from './DiscussionForm';
 import Comments from './Comments';
+import axios from 'axios';
 
 function ShowDiscussion(props) {
+
+    const [isLoading, setLoading] = useState(true)
+    const [discussion, setDiscussion] = useState();
+
+    useEffect(() => {
+        try{
+            const getDiscussion = async () => {
+                const response = await axios.get(
+                    `http://localhost:8000/api/discussions/${props.show.id}-${props.show.name}/discussion`
+                )
+                setDiscussion(response.data)
+                setLoading(false)
+            }
+            getDiscussion()
+        } catch(err) {
+            console.log("🚀 ~ file: ShowDiscussion.jsx ~ line 24 ~ useEffect ~ err", err)
+        }
+    }, []);
+
     
     return ( 
 
@@ -15,15 +35,25 @@ function ShowDiscussion(props) {
                         <h6 className="text-white"> Discussion </h6>
                     </Accordion.Header>
                     <Accordion.Body>
-                            <DiscussionForm {...props} />
-                            <Card>
-                                <Comments {...props} />
-                            </Card>
+                            {isLoading &&
+                                <Spinner animation="border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                                </Spinner>
+                            
+                            }
+                            {!isLoading &&
+                            <>
+                                <DiscussionForm {...props} hasDiscussion={discussion ? true : false} />
+                                <Card>
+                                    {/* <Comments {...props} userComments={discussion[0]} userReplies={discussion[1]} /> */}
+                                </Card>
+                            </>
+                            }
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
         </div>
-     );
+    );
 }
 
 export default ShowDiscussion;
