64d91ec02c7e8e16ecc2a4c29c92e9e8f331b8af
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-10 14:20:38 -0500

    Changed style of button.

--- src/components/PageShowDetails/UserRowIcons/PlayTrailerIcon.jsx
+++ src/components/PageShowDetails/UserRowIcons/PlayTrailerIcon.jsx
@@ -2,8 +2,8 @@ import { MDBBtn, MDBIcon } from "mdb-react-ui-kit"
 
 const PlayTrailerIcon = (props) => {
     return (
-        <MDBBtn size="lg" className="mx-2"> 
-            <i className="bi bi-youtube"></i> Play Trailer
+        <MDBBtn size="lg" className="mx-2 text-white" color="null"> 
+            <i class="fas fa-play me-1"></i> Play Trailer
         </MDBBtn>
     )
 }
