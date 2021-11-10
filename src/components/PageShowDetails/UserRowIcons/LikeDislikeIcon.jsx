51ac8fd6d3601122785f2500ccc93e2c7e43a247
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-10 17:35:12 -0500

    Added logic so user's can like/dislike a show.

--- src/components/PageShowDetails/UserRowIcons/LikeDislikeIcon.jsx
+++ src/components/PageShowDetails/UserRowIcons/LikeDislikeIcon.jsx
@@ -1,6 +1,23 @@
+import axios from "axios"
 import { MDBBtn } from "mdb-react-ui-kit"
 
 const LikeDislikeIcon = (props) => {
+
+    const handleClick = async () => {
+        try {
+            const jwt = localStorage.getItem('token')
+            const vote = props.likedShow ? "thumbs_down" : "thumbs_up"
+            await axios.patch(`http://127.0.0.1:8000/api/show/${vote}`, {
+                id: props.show.databaseID
+            }, {
+                headers: { Authorization: `Bearer ${jwt}`}
+            })
+            props.getWatchedShows()
+        } catch(err) {
+            console.log("🚀 ~ file: LikeDislikeIcon.jsx ~ line 17 ~ handleClick ~ err", err)
+        }
+    }
+
     return (
         <>
         <MDBBtn 
@@ -8,6 +25,8 @@ const LikeDislikeIcon = (props) => {
             size="lg" 
             className="mx-2"
             color={props.likedShow ? "primary" : "dark"}
+            disabled={!props.watchedShow}
+            onClick={handleClick}
         > 
             <span class="material-icons">thumb_up</span>
         </MDBBtn>
@@ -15,10 +34,18 @@ const LikeDislikeIcon = (props) => {
             floating 
             size="lg" 
             className="mx-2"
-            color={!props.likedShow && !props.likedShow === null ? "primary" : "dark"}
+            color={props.likedShow === false ? "primary" : "dark"}
+            disabled={!props.watchedShow}
+            onClick={handleClick}
         > 
             <span class="material-icons">thumb_down</span>
         </MDBBtn>
+        <br />
+        {!props.watchedShow && 
+            <p className="text-muted mt-2">
+                Please mark the show as watched to vote.
+            </p>
+        }
         </>
     )
 }
