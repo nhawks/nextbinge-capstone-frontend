48ddf4cc1df9d81072ab35a9887a0e96f1a052bd
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-11 01:01:39 -0500

    Initial creation of modal.

--- src/components/PageShowDetails/UserRowIcons/PlayTrailerIcon.jsx
+++ src/components/PageShowDetails/UserRowIcons/PlayTrailerIcon.jsx
@@ -1,10 +1,39 @@
-import { MDBBtn, MDBIcon } from "mdb-react-ui-kit"
+import React, { useState } from 'react';
+import { MDBBtn } from "mdb-react-ui-kit"
+import { Button, Modal } from 'react-bootstrap';
+
 
 const PlayTrailerIcon = (props) => {
+
+    const [showModal, setShowModal] = useState(false);
+
     return (
-        <MDBBtn size="lg" className="mx-2 text-white" color="null"> 
+        <>
+        <MDBBtn 
+            size="lg" 
+            className="mx-2 text-white" 
+            color="null"
+            onClick={() => setShowModal(true)}
+
+        > 
             <i class="fas fa-play me-1"></i> Play Trailer
         </MDBBtn>
+
+            <Modal
+            size="xl"
+            show={showModal}
+            onHide={() => setShowModal(false)}
+        >
+            <Modal.Header closeButton>
+            <Modal.Title>
+                {props.show.name} Trailer
+            </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {/*//TODO: YouTube Video Player here */}
+            </Modal.Body>
+        </Modal>
+        </>
     )
 }
 
