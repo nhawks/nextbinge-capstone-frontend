fdecdf377751899e053b9b4eed250aab16f4f217
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-10 22:57:10 -0500

    Added logic to icon so user's can add/remove a show from their watchlist.

--- src/components/PageShowDetails/UserRowIcons/WatchListIcon.jsx
+++ src/components/PageShowDetails/UserRowIcons/WatchListIcon.jsx
@@ -1,9 +1,56 @@
+import axios from "axios"
 import { MDBBtn } from "mdb-react-ui-kit"
 
+
 const WatchListIcon = (props) => {
+
+    const addToWatchlist = async () => {
+        try {
+            const jwt = localStorage.getItem("token")
+            await axios.post("http://localhost:8000/api/show/watchlist/show/add", 
+            {
+                tv_show_id: props.showID,
+                tv_show_data: props.show
+            }, {
+                headers: { Authorization: `Bearer ${jwt}`}
+            })
+            props.getWatchlist() 
+        } catch(err) {
+            console.log("🚀 ~ file: WatchedShowIcon.jsx ~ line 21 ~ handleClick ~ err", err)
+        }
+    }
+
+    const removeFromWatchlist = async () => {
+        try {
+            const jwt = localStorage.getItem("token")
+            await axios.delete("http://localhost:8000/api/show/watchlist/show/delete", {
+                headers: { Authorization: `Bearer ${jwt}`},
+                data: {
+                    id: props.watchlistID
+                }
+            })
+            props.getWatchlist()
+        } catch(err) {
+
+        }
+    }
+
+    const handleClick = () => {
+        if (props.inWatchlist) {
+            removeFromWatchlist()
+        } else {
+            addToWatchlist()
+        }
+    }
+    
     return (
         
-        <MDBBtn floating size="lg" className="mx-2" color="dark"> 
+        <MDBBtn 
+            floating size="lg" 
+            className="mx-2" 
+            color={props.inWatchlist ? "primary" : "dark"}
+            onClick={handleClick}
+        > 
             <i class="fas fa-bookmark fa-lg"></i>
         </MDBBtn>
     )
