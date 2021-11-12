25e31d168fceba93e946afa955bf61c0e900c2c4
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-12 14:43:26 -0500

    Home page now displays rows of series by genre.

--- src/components/PageHome/PageHome.jsx
+++ src/components/PageHome/PageHome.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import { RowRequest } from './RowRequests';
+import SeriesRow from './SeriesRow';
 
 const PageHome = (props) => {
     return (
-        <div>
-            Home
+        <div className="container-fluid col-11" >
+            <SeriesRow title="Trending" requestURL={RowRequest.getTrending} />
+            <SeriesRow title="Action" requestURL={RowRequest.getAction} />
+            <SeriesRow title="Comedy" requestURL={RowRequest.getComedy} />
+            <SeriesRow title="Sci-Fi & Fantasy" requestURL={RowRequest.getSciFantasy} />
+            <SeriesRow title="Drama" requestURL={RowRequest.getDrama} />
+            <SeriesRow title="Reality" requestURL={RowRequest.getReality} />
         </div>
     )
 
