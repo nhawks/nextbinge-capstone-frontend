e91db8fb5d8109c3f62425ed132d91ece884f2d5
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-12 16:42:30 -0500

    Initial rework of search page.

--- src/components/PageSearch/PageSearch.jsx
+++ src/components/PageSearch/PageSearch.jsx
@@ -1,13 +1,57 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import { Card, Form,} from 'react-bootstrap';
+import { MDBBtn } from 'mdb-react-ui-kit';
 
 const PageSearch = (props) => {
+
+    const [searchByTitle, setSearchByTitle] = useState(false);
+
+    useEffect(() => {
+        console.log("ran")
+    }, [searchByTitle]);
+
+    
+
     return (
-        <div>
-            Search
+        <div className="row justify-content-center mt-4">
+            <div className="col-2" align="center">
+                <Card>
+                    <Card.Header>Search By</Card.Header>
+                    <Card.Body>
+                        <div className="d-grid gap-2 mx-auto">
+                            <MDBBtn
+                                tag="a"
+                                rounded
+                                color={!searchByTitle ? "primary" : "dark"}
+                                onClick={() => setSearchByTitle(false)}
+                            >
+                                Genre
+                            </MDBBtn>
+                            <MDBBtn
+                                tag="a"
+                                rounded
+                                color={searchByTitle ? "primary" : "dark"}
+                                onClick={() => setSearchByTitle(true)}
+
+                            >
+                                Title
+                            </MDBBtn>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="col-8">
+                <Card>
+                    <Card.Header>Search Results</Card.Header>
+                    <Card.Body>
+
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
     )
-
 }
 
 
