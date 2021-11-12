63fe4ec0bc8754604274d979df0719e2df3d8d41
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-12 17:06:46 -0500

    Imported list of genres and added logic to display.

--- src/components/PageSearch/PageSearch.jsx
+++ src/components/PageSearch/PageSearch.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
-import { Card, Form,} from 'react-bootstrap';
+import { Card, FloatingLabel, Form,} from 'react-bootstrap';
 import { MDBBtn } from 'mdb-react-ui-kit';
+import { genres } from './GenreList';
 
 const PageSearch = (props) => {
 
-    const [searchByTitle, setSearchByTitle] = useState(false);
+    const [searchByTitle, setSearchByTitle] = useState(null);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         console.log("ran")
@@ -24,7 +26,7 @@ const PageSearch = (props) => {
                             <MDBBtn
                                 tag="a"
                                 rounded
-                                color={!searchByTitle ? "primary" : "dark"}
+                                color={!searchByTitle && searchByTitle !== null ? "primary" : "dark"}
                                 onClick={() => setSearchByTitle(false)}
                             >
                                 Genre
@@ -34,12 +36,30 @@ const PageSearch = (props) => {
                                 rounded
                                 color={searchByTitle ? "primary" : "dark"}
                                 onClick={() => setSearchByTitle(true)}
-
                             >
                                 Title
                             </MDBBtn>
                         </div>
                     </Card.Body>
+                    {!searchByTitle && (
+                        <Card.Body>
+                        <FloatingLabel controlId="genreSelector" label="Genres">
+                        <Form.Select className="bg-dark text-white">
+                            <option  selected disabled value="">Select a Genre...</option>
+                            {genres.map((genre) => (
+                                <option value={genre.id}>{genre.name}</option>
+                            ))}
+                        </Form.Select>
+                        </FloatingLabel>
+                        </Card.Body>
+                        
+
+                    )}
+                    {searchByTitle && (
+
+                        null
+
+                    )}
                 </Card>
             </div>
             <div className="col-8">
