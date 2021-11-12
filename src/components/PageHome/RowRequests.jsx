13f79a6635fdd6126f9bf04fc9380c44e8be7ff1
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-12 14:42:42 -0500

    Created endpoints to handle search results  and home page.

---
+++ src/components/PageHome/RowRequests.jsx
@@ -0,0 +1,11 @@
+import { TMDB_API_KEY } from '../../keys'
+
+export const RowRequest = {
+    getTrending: `trending/tv/week?api_key=${TMDB_API_KEY}&language=en-US&page=1`,
+    getAction: `discover/tv?api_key=${TMDB_API_KEY}&language=en-US&page=1&with_genres=10759&watch_region=US`,
+    getComedy: `discover/tv?api_key=${TMDB_API_KEY}&language=en-US&page=1&with_genres=35&watch_region=US`,
+    getSciFantasy: `discover/tv?api_key=${TMDB_API_KEY}&language=en-US&page=1&with_genres=10765&watch_region=US`,
+    getDrama: `discover/tv?api_key=${TMDB_API_KEY}&language=en-US&page=1&with_genres=18&watch_region=US`,
+    getReality: `discover/tv?api_key=${TMDB_API_KEY}&language=en-US&page=1&with_genres=10764&watch_region=US`,
+}
+
