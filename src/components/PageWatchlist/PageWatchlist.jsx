48e87ab6fc4d1561c7701fe29344f7524815a7dc
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-11-13 17:27:13 -0500

    Added function so user's can remove shows from their watchlist.

--- src/components/PageWatchlist/PageWatchlist.jsx
+++ src/components/PageWatchlist/PageWatchlist.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Row } from 'react-bootstrap';
+import axios from 'axios';
+import { MDBBtn } from 'mdb-react-ui-kit';
 
 
 function PageWatchlist(props) {
-    const baseURL = "https://api.themoviedb.org/3/"
     const imageURL = "https://image.tmdb.org/t/p/w500"
+
+    async function removeFromWatchlist (watchlistID) {
+        try {
+            const jwt = localStorage.getItem("token")
+            await axios.delete("http://localhost:8000/api/show/watchlist/show/delete", {
+                headers: { Authorization: `Bearer ${jwt}`},
+                data: {
+                    id: watchlistID
+                }
+            })
+            props.getWatchlist()
+        } catch(err) {
+            console.log("🚀 ~ file: PageWatchlist.jsx ~ line 20 ~ removeFromWatchlist ~ err", err)
+        }
+    }
 
     return (
         <div className="container mt-4 mx-auto">
@@ -15,14 +31,17 @@ function PageWatchlist(props) {
             <Row xs={2} md={4} lg={5} className="g-4">
                 {props.watchlist.map((show) => (
                     <div className="text-center">
+                        <Card style={{ width: '180px' }} className="me-4">
                         <Link to="/show-details" onClick={() => props.setShowID(show.tv_show_id)} >
-                        <Card style={{ width: '200px' }} className="me-4">
                         <Card.Img variant="top" src={`${imageURL}${show.tv_show_data.poster_path}`} />
+                        </Link>
                         <Card.Body>
-                            <Card.Title>{show.tv_show_data.name}</Card.Title>
+                            {/* <Card.Title>{show.tv_show_data.name}</Card.Title> */}
+                            <MDBBtn rounded onClick={() => removeFromWatchlist(show.id)}>
+                                Remove
+                            </MDBBtn>
                         </Card.Body>
                         </Card>
-                        </Link>
                     </div>
                 ))}
             </Row>
